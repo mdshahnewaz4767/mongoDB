3ffa77fb72fead4a645cce2cf7ae911f05de2fa5
shahnewaz601 <shahnewaz601@gmail.com>
2021-03-24 01:22:29 +0600

    Connect to cloud database introduction to CRUD

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = "mongodb+srv://organicUser:NewazZ@1010*@cluster0.mkcgo.mongodb.net/organicdb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
@@ -6,4 +10,11 @@ app.get('/', (req, res) => {
     res.send('I am working');
 })
 
-app.listen(3000);
+client.connect(err => {
+  const collection = client.db("organicdb").collection("products");
+  // perform actions on the collection object
+  console.log('database connected');
+  client.close();
+});
+
+app.listen(3001);
